26307f2a666229f2d6a8dfacb013b28f784ca13e
Sven Lütje <sven.luetje@googlemail.com>
2016-12-21 01:17:32 +0100

    fix namespaces and add basic unit test for user entity

---
+++ Test/codeception/_support/Helper/Unit.php
@@ -0,0 +1,10 @@
+<?php
+namespace B4rb4ross4\Bundle\UserBundle\Test\Helper;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class Unit extends \Codeception\Module
+{
+
+}
---
+++ Test/codeception/unit/UserCest.php
@@ -0,0 +1,19 @@
+<?php
+namespace UserBundle;
+use UserBundle\UnitTester;
+
+class UserCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    public function _after(UnitTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(UnitTester $I)
+    {
+    }
+}
