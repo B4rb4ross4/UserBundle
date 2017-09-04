7927f184c2011b221c8aed0c31cad1140d41bb8a
Sven Lütje <sluetje@eos-uptrade.de>
2017-09-04 20:02:37 +0200

    renamed bundle class according to https://symfony.com/doc/current/bundles/best_practices.html

--- B4rb4ross4UserBundle.php
+++ B4rb4ross4UserBundle.php
@@ -6,7 +6,7 @@ use UserBundle\DependencyInjection\Compiler\OverrideServiceCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class UserBundle extends Bundle
+class B4rb4ross4UserBundle extends Bundle
 {
 
 }
